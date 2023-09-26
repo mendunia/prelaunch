d4b7cc6e45e460c561386281c592e38220f5a2be
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-09-26 12:12:04 +0700

    add contact page

--- src/app/components/footerImage.tsx
+++ src/app/components/footerImage.tsx
@@ -2,13 +2,24 @@
 
 import { usePathname } from 'next/navigation'
 import Image from 'next/image'
+import Button from '@/components/button'
 export default function FooterImage()
 {
     const pathname = usePathname()
 
     if(pathname === "/")
     return (
-        <Image height={250} width={250} src="/hero/questioning.png" alt='questioning people image'/>
+        <div className='flex flex-col gap-3'>
+            <Image height={250} width={250} src="/hero/questioning.png" alt='questioning people image'/>
+
+            <h1 className='text-white font-semibold text-2xl text-center'>Punya Pertanyaan?</h1>
+
+            <div className='flex justify-center'>
+                <Button type={"Secondary"} text={"Tanya Disini"} />
+
+            </div>
+
+        </div>
     )
 
     return <></>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,9 +2,10 @@ type ButtonType = "Primary" | "Secondary"
 
 export default function Button (props : {text: String, type? : ButtonType})
 {
+    console.log(props.type)
     return (
-        <div className={`py-2.5 px-4 ${props.type === "Secondary" ? "bg-white" : "bg-red-800"} rounded-full justify-center items-center gap-2 inline-flex`}>
-            <div className={`text-very-light-grey ${props.type === "Secondary" ? "text-red-900" : "text-white"} text-xs font-normal`}>{props.text}</div>
+        <div className={`py-2.5 px-4 ${props.type == "Secondary" ? "bg-white" : "bg-red-800"} rounded-full justify-center items-center gap-2 inline-flex`}>
+            <div className={`${props.type == "Secondary" ? "text-merah-inti" : "text-very-light-grey"} text-xs font-normal`}>{props.text}</div>
         </div>    
     )
 }
