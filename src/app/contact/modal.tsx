e7e5bd74abe149853d6f5327ac74572a2796e0a0
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-09-26 21:04:35 +0700

    polish contact popup layout

--- src/app/contact/modal.tsx
+++ src/app/contact/modal.tsx
@@ -9,10 +9,9 @@ export default function PopUpModal(props : { openModal : boolean | undefined, se
 
   return (
     <>
-      <Modal className='grid place-content-center items-center bg-black bg-opacity-50 transition-all' show={props.openModal} size="md" popup onClose={() => props.setOpenModal(false)}>
-        <div className='w-64'>
-            <Modal.Body>
-            <div className='w-full h-full flex flex-col gap-5 py-5 justify-center items-center'>
+      <Modal theme={{content: {inner: "rounded-3xl bg-white"}}} className='grid place-content-center items-center bg-black bg-opacity-50 transition-all' show={props.openModal} size="md" popup onClose={() => props.setOpenModal(false)}>
+            <Modal.Body className='w-64 '>
+            <div className='px-5 w-full h-full flex flex-col gap-5 py-5 justify-center items-center'>
                 <Image src="/icons/paperplane.svg" width={50} height={50} alt='icon sent' loading='eager'/>
                 <p className='text-center text-sm'> Terima kasih!<br/>
                     Pesan kamu telah terkirim, kami akan segera membalas pesanmu melalui email. 
@@ -23,7 +22,12 @@ export default function PopUpModal(props : { openModal : boolean | undefined, se
 
             </div>
             </Modal.Body>
-        </div>
+            <Modal.Footer className='flex flex-col gap-1 rounded-b-full'>
+            <div className='h-1 w-full bg-red-800'/>
+            <div className='rounded-b-3xl h-4 w-full bg-red-800'/>
+
+            </Modal.Footer>
+
       </Modal>
     </>
   )
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,6 @@ type ButtonType = "Primary" | "Secondary"
 
 export default function Button (props : {text: String, type? : ButtonType})
 {
-    console.log(props.type)
     return (
         <div className={`py-2.5 px-4 ${props.type == "Secondary" ? "bg-white" : "bg-red-800"} rounded-full justify-center items-center gap-2 inline-flex`}>
             <div className={`${props.type == "Secondary" ? "text-merah-inti" : "text-very-light-grey"} text-xs font-normal`}>{props.text}</div>
