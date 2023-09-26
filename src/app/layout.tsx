c336af0ab079785ac600b38e6295de7f7cae3e1b
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-09-26 11:28:14 +0700

    finish mobile layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import Image from 'next/image'
+import Link from 'next/link'
+import FooterImage from './components/footerImage'
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700', '800'],
@@ -22,8 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
-      
-      <body suppressHydrationWarning className={poppins.className + " py-6"}>
+      <body suppressHydrationWarning className={poppins.className + " pt-6 flex flex-col gap-5"}>
         <nav className='flex flex-col items-center gap-6 px-8 py-6'>
         <Image src={"/company/logo.png"} className='w-48' height={25} width={100} alt='Mendunia Logo'/>
           <div className='flex flex-col items-center justify-center text-center gap-3'>
@@ -32,7 +33,36 @@ export default function RootLayout({
             <Button text={"Pelajari lebih lanjut"}/>
           </div>
         </nav>
+
         {children}
+
+        <div className="w-full h-full flex flex-col gap-7 justify-center items-center bg-gradient-to-b from-red-700 via-red-700 to-red-950 py-7">
+          <FooterImage/>
+
+          <h1 className='text-white font-semibold text-2xl text-center'>Punya Pertanyaan?</h1>
+          
+          <Button type={"Secondary"} text={"Tanya Disini"} />
+
+          <Image src={"/company/logo-white.png"} width={200} height={50} alt='Company Logo'/>
+          <div className='flex gap-5'>
+            {["linkedin", "facebook", "instagram"].map((social, i) => <Image 
+              key={`social logo ${i}`}
+              src={`/icons/${social}.png`}
+              alt={`our ${social} account`}
+              width={25}
+              height={25}
+              />)}
+          </div>
+
+
+          {["Tentang Mendunia", "Hubungi Kami", "Daftarkan Agensi Anda", "Syarat dan Ketentuan"].map((link, i) => 
+            <Link className='text-white' key="link" href={"/link"}>
+              {link}
+            </Link>)}
+
+            <p className='inline-flex justify-center items-center text-white underline gap-1'><Image className='mt-1.5' width={20} height={1} src={"/icons/indo.png"} alt='Current Language'/>Bahasa</p>
+        </div>
+
       </body>
     </html>
   )
