0dbda9cbb9218538031ea381b77a5ff50eb47e6a
Imam Syahid Hudzaifah <imaamsyahid@gmail.com>
2023-09-29 16:18:18 +0700

    finish about desktop layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,7 +23,7 @@ export default function Page()
         }
       ]
 
-    return <div className="animate__animated animate__fadeInLeft  gap-10 flex flex-col justify-center items-center">
+    return <div className="animate__animated animate__fadeInLeft  gap-10 lg:gap-14 flex flex-col justify-center items-center">
         
         <div className="bg-grey flex flex-col gap-10 max-sm:py-8 ">
           <div className="max-sm:hidden flex justify-center w-full">
@@ -32,8 +32,8 @@ export default function Page()
 
           <div className="md:grid md:grid-cols-2 md:gap-10 px-8 md:pb-10">
           <div className="flex flex-col justify-center items-center gap-4 md:gap-5">
-              <h1 className=" text-xl md:text-2xl xl:text-3xl md:text-left md:self-start text-merah-inti font-semibold">Apa itu Mendunia?</h1>
-              <p className="text-xs text-center md:text-left leading-5">
+              <h1 className=" text-xl md:text-2xl lg:text-5xl md:text-left md:self-start text-merah-inti font-semibold">Apa itu Mendunia?</h1>
+              <p className="text-xs lg:text-base text-center md:text-left  leading-5">
                   Temukan peluang pendidikan global melalui Mendunia, platform inovatif yang mendukung pelajar Indonesia mewujudkan impian kuliah di luar negeri. 
                   Dengan jangkauan luas di seluruh Indonesia, Mendunia memudahkan pencarian agensi pendidikan yang sesuai, dari yang dekat hingga lokasi tertentu. 
                   Komunikasi langsung dengan agensi terpilih semakin mudah di Mendunia, dilengkapi dengan informasi lengkap tentang universitas-universitas di dunia memberi 
@@ -42,29 +42,29 @@ export default function Page()
               </p>
               <Button text={"Daftarkan agensi anda. Gratis!"}/>
           </div>
-          <div className="max-sm:hidden py-14 flex justify-center">
-            <Image className="py-10" src="/hero/banner-logo.png" width={500} height={200} alt="banner"/>
+          <div className="max-sm:hidden py-14  lg:bg-gray-100 rounded-2xl flex justify-center ">
+            <Image className="py-14 lg:py-10" src="/hero/banner-logo.png" width={500} height={200} alt="banner"/>
           </div>
           </div>
         </div>
        
         
 
-      <div className="flex flex-col gap-5 max-sm:w-3/4 max-sm:pb-14 md md:gap-10 justify-center items-center md:px-8">
-        <h1 className="text-xl md:text-2xl xl:text-3xl text-merah-inti font-semibold text-center md:self-start  ">Apa saja yang bisa anda lakukan?</h1>
+      <div className="flex flex-col gap-5 lg:gap-20 max-sm:w-3/4 max-sm:pb-14 md md:gap-10 justify-center items-center md:px-8">
+        <h1 className="text-xl md:text-2xl lg:text-5xl text-merah-inti font-semibold text-center md:self-start  ">Apa saja yang bisa anda lakukan?</h1>
         <Carousel className="flex overflow-auto md:grid md:grid-cols-3 " data={["s"]} id='Keuntungan'>
                 {keutunganData.map((keuntungan, i) => <div className={`min-w-full max-sm:max-w-[50%] text-center flex flex-col gap-3 justify-center md:justify-start items-center min-h-full  `} key={"Keuntungan " + i}>
-                    <Image className="md:h-14" src={keuntungan.imageUrl} width={50} height={50} alt={keuntungan.title}/>
-                    <h1 className='font-semibold text-dark-grey'>{keuntungan.title}</h1>
-                    <p className='text-xs leading-5 px-4'>{keuntungan.desc}</p>
+                    <Image className="md:w-14 md:h-16 lg:w-24 lg:h-32 lg:pb-8" src={keuntungan.imageUrl} width={250} height={50} alt={keuntungan.title}/>
+                    <h1 className='font-semibold lg:text-2xl text-dark-grey'>{keuntungan.title}</h1>
+                    <p className='text-xs lg:text-base leading-5 px-4'>{keuntungan.desc}</p>
                 </div>)}
             </Carousel>
       </div>
 
       <div className='relative w-full pb-5 max-sm:hidden'>
-        <Image src='/hero/banner-qna.png' width={1280} height={360} alt='qna-background'/>
-        <div className='absolute left-5 top-8 flex flex-col gap-6'>
-          <h1 className='text-xl md:text-2xl xl:text-3xl font-semibold text-white md:w-3/4'>Masih punya pertanyaan tentang kami?</h1>
+        <Image className="w-full" src='/hero/banner-qna.png' width={1280} height={360} alt='qna-background'/>
+        <div className='absolute left-5 lg:left-10 top-8 lg:top-16 flex flex-col gap-6'>
+          <h1 className='text-xl md:text-2xl lg:text-5xl lg:leading-snug font-semibold text-white md:w-3/4'>Masih punya pertanyaan tentang kami?</h1>
           <Link href={"/contact"}>
             <Button type='Secondary' text={"Tanya disini"} />
           </Link>
